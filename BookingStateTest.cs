7ecd1c9f89d1ea65289ae1642c048728db6ad6d1
farhangold <121116261+farhangold@users.noreply.github.com>
2023-05-06 20:11:08 +0700

    unit Test

---
+++ BookingStateTest.cs
@@ -0,0 +1,44 @@
+﻿using mentoring_system;
+using mentoring_system.controller;
+using static mentoring_system.controller.bookingState;
+
+namespace unit_test_mentor
+{
+    [TestClass]
+    public class BookStateMachineTests
+    {
+
+        [TestMethod]
+        public void TestGetNextState()
+        {
+            // Arrange
+            bookState currentState = bookState.NONE;
+            bookTrigger trigger = bookTrigger.REGISTER;
+
+            bookingState stateMachine = new bookingState();
+
+
+            // Act
+            bookState finalState = stateMachine.GetNextState(currentState, trigger);
+
+            // Assert
+            Assert.AreEqual(bookState.READY, finalState, "The final state is not as expected.");
+        }
+
+        /*[TestMethod]
+        public void TestActivateTrigger()
+        {
+            // Arrange
+            bookState currentState = bookState.NONE;
+            bookTrigger trigger = bookTrigger.REGISTER;
+            bookingState stateMachine = new bookingState();
+
+            // Act
+            stateMachine.ActivateTrigger(trigger);
+
+            // Assert
+            Assert.AreEqual(bookState.READY, stateMachine.currentState, "The current state is not as expected.");
+        }*/
+    }
+
+}
--- LoginTest.cs
+++ LoginTest.cs
@@ -12,11 +12,19 @@ namespace unit_test_mentor
             bool admin = login.isAdmin("admin" , "admin");
             Assert.IsTrue(admin);
         }
-
+        [TestMethod]
         public void isAdminFalse()
         {
             LoginMentee login = new LoginMentee();
             bool admin = login.isAdmin("user", "password");
+            Assert.IsFalse(admin);
+        }
+
+        [TestMethod]
+        public void isPaaswordLengthSufficient()
+        {
+            LoginMentee login = new LoginMentee();
+            bool admin = login.isPasswordEightCharacter("strongpassword");
             Assert.IsFalse(admin);
         }
     }
