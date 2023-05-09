2fc70f2577702516f361f361567f553c72ef04b3
LAPTOP-GTQCMDG9\Asus <vidyaratmayanti@gmail.com>
2023-05-09 12:44:18 +0700

    unit testing profile

--- LoginTest.cs
+++ LoginTest.cs
@@ -1,5 +1,7 @@
 
 using mentoring_system;
+using mentoring_system.view;
+
 namespace unit_test_mentor
 {
     [TestClass]
@@ -9,15 +11,37 @@ namespace unit_test_mentor
         public void isAdminTrue()
         {
             LoginMentee login = new LoginMentee();
-            bool admin = login.isAdmin("admin" , "admin");
+            bool admin = login.isAdmin("admin", "admin");
             Assert.IsTrue(admin);
         }
-
+        [TestMethod]
         public void isAdminFalse()
         {
             LoginMentee login = new LoginMentee();
             bool admin = login.isAdmin("user", "password");
             Assert.IsFalse(admin);
         }
+
+        [TestMethod]
+        public void IsUsernameValid_With_Uppercase_Letter()
+        {
+
+            ProfilePageMentee user = new ProfilePageMentee();
+
+            bool result = user.IsUsernameValid("UserName");
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void IsUsernameValid_With_No_Uppercase_Letter()
+        {
+            ProfilePageMentee user = new ProfilePageMentee();
+
+            bool result = user.IsUsernameValid("username");
+
+            Assert.IsFalse(result);
+        }
+
     }
 }
