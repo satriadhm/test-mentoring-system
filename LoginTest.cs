8f5028412f9c125203ba19226e81c44967ba4939
flxnzz <itszyro.47@gmail.com>
2023-05-09 12:15:13 +0700

    menambahkan test case Haikal

--- LoginTest.cs
+++ LoginTest.cs
@@ -1,10 +1,13 @@
 
 using mentoring_system;
+using mentoring_system.view;
+
 namespace unit_test_mentor
 {
     [TestClass]
     public class LoginTest
     {
+        // Test case Satria
         [TestMethod]
         public void isAdminTrue()
         {
@@ -13,11 +16,43 @@ namespace unit_test_mentor
             Assert.IsTrue(admin);
         }
 
+        [TestMethod]
         public void isAdminFalse()
         {
             LoginMentee login = new LoginMentee();
             bool admin = login.isAdmin("user", "password");
             Assert.IsFalse(admin);
         }
+
+        // Test case Haikal
+        [TestMethod]
+        public void isValidSubject()
+        {
+            LoginMentor Mentor = new LoginMentor();
+            String namaMatkul = "Data Structure";
+            Assert.IsTrue(Mentor.isValidSubject(namaMatkul));
+        }
+
+        [TestMethod]
+        public void checkRole()
+        {
+            LoginMentor login = new LoginMentor();
+            Assert.IsTrue(login.isMentor("Rafidhia Haikal"));
+        }
+
+        // Test case Vidya
+        //[TestMethod]
+        public void IsUsernameValid_Should_Return_False_For_Username_With_Uppercase_Letter()
+            {
+                // Arrange
+                ProfilePageMentee user = new ProfilePageMentee();
+
+                // Act
+                bool result = user.IsUsernameValid("UserName");
+
+                // Assert
+                Assert.IsTrue(result);
+            }
+
     }
 }
